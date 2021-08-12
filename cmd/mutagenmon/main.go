f046e248b54507e4c13114d27a8128802dc4fbdb
andy <andy@desktop.andmed.org>
2021-08-12 17:36:10 +0300

    add mutagen initialization

--- cmd/mutagenmon/main.go
+++ cmd/mutagenmon/main.go
@@ -4,6 +4,7 @@ import (
 	"go.andmed.org/mutagenmon"
 	"io/ioutil"
 	"log"
+	"os/exec"
 	"time"
 )
 
@@ -16,12 +17,21 @@ func main() {
 		log.SetOutput(out)
 	}
 
+	cmd := exec.Command("/usr/local/bin/mutagen", "list")
+
+
 	var mm *mutagenmon.MutagenMon
 	for {
+		err := cmd.Run()
+		if err != nil {
+			log.Printf("[ERROR] %s\n", err)
+		}
+
 		mm, err = mutagenmon.New()
 		if err == nil {
 			break
 		}
+		log.Printf("[Info] waiting for initialization\n")
 		time.Sleep(mutagenmon.IntervalSec * time.Second)
 	}
 	mm.Run()
