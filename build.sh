e6cca260a38907b94b86e7d04965104d2665e495
andy <andy@desktop>
2022-02-13 01:28:15 +0300

    notarize build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #/bin/sh
+
 mkdir -p MutagenMon.app/Contents/MacOS
-export TERM=dumb
-go build go.andmed.org/mutagenmon/cmd/mutagenmon && mv mutagenmon MutagenMon.app/Contents/MacOS/
+go build -buildmode=pie  go.andmed.org/mutagenmon/cmd/mutagenmon && mv mutagenmon MutagenMon.app/Contents/MacOS/
--- sign.sh
+++ sign.sh
@@ -1,4 +1,8 @@
 #/bin/sh
+set -e
 
 security find-identity -v -p codesigning
-codesign -s "Apple Distribution: Andrey Medvedev (BPN9958X73)" --verbose=4 MutagenMon.app
+codesign --force --options=runtime --timestamp -s "Developer ID Application: Andrey Medvedev (BPN9958X73)" --verbose=2 MutagenMon.app
+ditto -c -k --keepParent MutagenMon.app MutagenMon.app.zip 
+xcrun notarytool submit MutagenMon.app.zip   --keychain-profile "MutagenMon"    --wait
+spctl --assess --verbose MutagenMon.app
