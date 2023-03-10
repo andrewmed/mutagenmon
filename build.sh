d2dc165d6fcc8b4897bbac594a225e13f35a64f5
Serhii Doroshenkov <serhii.doroshenkov@spryker.com>
2023-03-10 18:30:21 +0100

    Actualise build for OSX monterey and go dependencies for last build.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 mkdir -p MutagenMon.app/Contents/MacOS
-GOOS=darwin GOARCH=arm64 go build -buildmode=pie  go.andmed.org/mutagenmon/cmd/mutagenmon && mv mutagenmon MutagenMon.app/Contents/MacOS/
+GOOS=darwin go build -buildmode=pie  go.andmed.org/mutagenmon/cmd/mutagenmon && mv mutagenmon MutagenMon.app/Contents/MacOS/
